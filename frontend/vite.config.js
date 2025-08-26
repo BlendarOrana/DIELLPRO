5e2cf7c7260717c15abe93b55efc18dcb03a3b42
BlendarOrana <blendorana1@hotmail.com>
2025-08-26 20:39:20 +0200

    push this bitch

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,11 @@
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/', // <- ensure this matches your deployment root
   server: {
     port: 3000,
     proxy: {
