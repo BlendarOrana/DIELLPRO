e56a7db32894ad84c2392f65ccf4934835efa4a5
BlendarOrana <blendorana1@hotmail.com>
2025-08-26 23:02:47 +0200

    let s goo

--- backend/server.js
+++ backend/server.js
@@ -65,7 +65,7 @@ if(process.env.NODE_ENV === "production"){
 
 
     app.get("*",(req,res)=>{
-        res.sendFile(path.resolve(__dirname,"frontend","dist"))
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
     })
 }
 
